daa589b28b4e3d3972c44dda82e8c5fa842f9b8e
akinyiliz <lizzieke84@gmail.com>
2024-07-04 23:41:17 +0300

    feat: Implement endpoint logic

---
+++ weather/views.py
@@ -0,0 +1,65 @@
+import os
+import requests
+from dotenv import load_dotenv
+from django.http import JsonResponse
+from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.core.exceptions import ImproperlyConfigured
+
+load_dotenv()
+
+
+class HelloView(View):
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+    def get(self, request):
+        visitor_name = request.GET.get('visitor_name', 'Guest')
+        ip_info_token = os.getenv("IP_INFO_TOKEN")
+        weather_api_key = os.getenv("WEATHER_API_KEY")
+
+        if not ip_info_token or not weather_api_key:
+            raise ImproperlyConfigured(
+                "API keys not set in environment variables.")
+
+        # Get visitor's IP address
+        visitor_ip = self.get_client_ip(request)
+
+        response = requests.get(
+            f"https://ipinfo.io/json?token={ip_info_token}")
+        data = response.json()
+
+        location = data["loc"].split(",")
+        lat, lon = location[0], location[1]
+
+        try:
+            # Fetch weather information from OpenWeatherMap based on latitude and longitude
+            weather_response = requests.get(
+                f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={weather_api_key}"
+            )
+            weather_response.raise_for_status()
+            weather_data = weather_response.json()
+
+            city = weather_data["name"]
+            temp = int(weather_data["main"]["feels_like"])
+
+            # Return response with visitor's IP, location, and greeting message
+            return JsonResponse({
+                "client_ip": visitor_ip,
+                "location": city,
+                "greeting": f"Hello, {visitor_name}!, the temperature is {temp} degrees Celsius in {city}"
+            })
+
+        except requests.exceptions.RequestException as e:
+            # Handle errors related to HTTP requests
+            return JsonResponse({"error": "Error fetching data from external API"}, status=500)
+
+        except KeyError as e:
+            # Handle errors related to missing data in the API response
+            return JsonResponse({"error": "Missing data in the API response"}, status=500)
