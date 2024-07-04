949e3a210b19ed37dac6dadfd910c4656ced2ce1
akinyiliz <lizzieke84@gmail.com>
2024-07-05 00:12:45 +0300

    chore: Update Django settings and Vercel configuration

--- django_weather_greeting/wsgi.py
+++ django_weather_greeting/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_weather_greeting.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                      "django_weather_greeting.settings")
 
 application = get_wsgi_application()
+
+app = application
