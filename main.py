34690b32e231cc27407e5285e42d7fcdfa012f13
akinyiliz <lizzieke84@gmail.com>
2024-07-04 22:34:45 +0300

    feat: Update API endpoint to retrieve visitor's IP location and weather information

--- main.py
+++ main.py
@@ -20,34 +20,33 @@ async def get_visitor(visitor_name: str,  request: Request):
     Endpoint to greet a visitor with their name and weather information based on their IP location.
     """
 
+    x_forwarded_for = request.headers.get('X-Forwarded-For')
+    if x_forwarded_for:
+        visitor_ip = x_forwarded_for.split(',')[0]
+    else:
+        visitor_ip = request.client.host
+
     try:
         # Retrieve visitor's IP address and location info
         response = requests.get(
             f"https://ipinfo.io/json?token={IP_INFO_TOKEN}")
         data = response.json()
 
-        visitor_ip = data["ip"]
-        location = data["loc"].split(",")
+        city = data["city"]
 
-        lat = location[0]
-        lon = location[1]
-
-        # Fetch weather information from OpenWeatherMap based on latitude and longitude
-        weather_response = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={WEATHER_API_KEY}")
-
-        # Check if the request was successful
-        weather_response.raise_for_status()
-
-        weather_data = weather_response.json()
-
-        city = weather_data["name"]
-        temp = int(weather_data["main"]["feels_like"])
+        # Fetch weather information from WeatherAPI based on city
+        response = requests.get(
+            f"https://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={city}")
+        if response.status_code == 200:
+            data = response.json()
+            temperature = data['current']['temp_c']
+        else:
+            return "data unavailable"
 
         # Return response with visitor's IP, location, and greeting message
         return {"client_ip": visitor_ip,
                 "location": city,
-                "greeting": f"Hello, {visitor_name}!, the temperature is {temp} degrees Celcius in {city}"
+                "greeting": f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celcius in {city}"
                 }
 
     except requests.exceptions.RequestException as e:
